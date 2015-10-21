fae5575a026452a7784533fbbdd5e764f3e11c81
lxh5147 <lxh5147@gmail.com>
2015-10-20 21:44:10 -0400

    Remove debug helper functions

--- tools/import_theano_model.hpp
+++ tools/import_theano_model.hpp
@@ -148,33 +148,6 @@ namespace nn_tools {
             JsonUtil(){}
     };
 
-    //debug helper functions
-    template<typename T>
-    void dump(const Vector<T>& vector){
-        cout<<"size="<<vector.size()<<endl;
-        T* data=vector.data().get();
-        for(size_t i=0;i<vector.size();++i){
-            cout<<data[i]<<" ";
-        }
-        cout<<endl;
-    }
-
-    template<typename T>
-    void dump(const Matrix<T>& matrix){
-        size_t row=matrix.row();
-        size_t col=matrix.col();
-        cout<<"row="<<row<<","<<"col="<<col<<endl;
-        T* data=matrix.data().get();
-        cout.setf(ios::fixed,ios::floatfield);
-        cout.precision(5);
-        for(size_t i=0;i<row;++i){
-            for(size_t j=0;j<col;++j){
-               cout<<data[i*col+j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
-
     //Defines theano MLP models, which are saved into a folder.
     template <typename T>
     class TheanoModel{
