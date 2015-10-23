a6b5588289f7efd92644b7f9a4d29406ea363703
lxh5147 <lxh5147@gmail.com>
2015-10-23 13:46:49 -0400

    Added comments to jni functions

--- jni/nn_runtime.h
+++ jni/nn_runtime.h
@@ -2,9 +2,11 @@
 using namespace std;
 
 //Loads the binary model into memory. Returns the handle of the loaded model or HANDLE_INVALID if it failed to load the model.
+//Parameters:modelPath represents the binary model stored in disk; normalizeOutputWithSoftmax indicates whether or not the outputs should be normalized with softmax
 extern size_t load(const char* modelPath,bool normalizeOutputWithSoftmax=true);
 
 //Predicts a probability for each category. The input is a list of id sequences. For a non-sequence input, there must be one element in the id sequence.
+//Parameters: modelHandler represents the handle of the runtime model that has been loaded; idsInputs represents the list of id sequences which are the inputs of the runtime.
 //Note: leave a blank between vector< vector<> > to make swig happy
-extern vector<double> predict(size_t modelHandle, const vector< vector<size_t> >& idsInputs);//
+extern vector<double> predict(size_t modelHandle, const vector< vector<size_t> >& idsInputs);
 
